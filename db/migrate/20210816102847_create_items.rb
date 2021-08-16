52fefab3cb469469caac49e05c126958b4d6b59b
yamamoto-hirofumi <sun.lionking.88@gmail.com>
2021-08-16 11:37:53 +0000

    モデル作成、カラム追加

---
+++ db/migrate/20210816102847_create_items.rb
@@ -0,0 +1,14 @@
+class CreateItems < ActiveRecord::Migration[5.2]
+  def change
+    create_table :items do |t|
+      t.integer :genre_id
+      t.string :name
+      t.text :introduction
+      t.string :image_id
+      t.integer :price
+      t.boolean :is_sales_status
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_16_093119) do
+ActiveRecord::Schema.define(version: 2021_08_16_102847) do
 
   create_table "customers", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,6 +30,17 @@ ActiveRecord::Schema.define(version: 2021_08_16_093119) do
     t.boolean "is_secede_frag"
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.integer "genre_id"
+    t.string "name"
+    t.text "introduction"
+    t.string "image_id"
+    t.integer "price"
+    t.boolean "is_sales_status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
