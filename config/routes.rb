bb25aa4d8932f8cb9ecce2a6f54fbdcd3c80645f
hamo-ss <192932aqua@gmail.com>
2021-08-16 11:35:44 +0000

    [add]ジャンルモデル追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :admin do
+    resources :genres, except: [:show, :destroy, :new]
+  end
 end
