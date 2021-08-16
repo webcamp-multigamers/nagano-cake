2718b81fde12da81dfb7eae35c9bbdccffbeaf71
hamo-ss <192932aqua@gmail.com>
2021-08-16 12:26:56 +0000

    [add]ジャンル一覧ページ・追加機能

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -6,6 +6,13 @@ class Admin::GenresController < ApplicationController
 
   def create
     @genre = Genre.new(genre_params)
+    if @genre.save
+      redirect_to request.referer
+    else
+      @genre = Genre.new
+      @genres = Genre.all
+      render :index
+    end
   end
 
   def edit
