4fd16d137bc46f23398d33428ce980d0e68d6aae
Sanzaka <equation0815@icloud.com>
2021-08-17 09:28:14 +0000

    [fix]コントローラ参照先の修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   devise_for :admins, controllers: {
-    sessions: 'admin/admins/sessions',
-    passwords: 'admin/admins/passwords',
-    registrations: 'admin/admins/registrations'
+    sessions: 'admin/sessions',
+    passwords: 'admin/passwords',
+    registrations: 'admin/registrations'
   }
   devise_for :customers, controllers: {
-    sessions: "customers/sessions",
-    passwords: "customers/passwords",
-    ragistrations: "customers/registrations"
+    sessions: "customer/sessions",
+    passwords: "customer/passwords",
+    ragistrations: "customer/registrations"
   }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
