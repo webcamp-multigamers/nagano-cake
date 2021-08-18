3b4d5f79b489d93f19f301f975a334bf092a7e18
Sanzaka <equation0815@icloud.com>
2021-08-18 02:57:20 +0000

    [add]customerのサインイン、サインアップページの完成、is_secede_fragのデフォルト値の追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_17_025954) do
+ActiveRecord::Schema.define(version: 2021_08_18_024911) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -39,7 +39,7 @@ ActiveRecord::Schema.define(version: 2021_08_17_025954) do
     t.string "postal_code"
     t.string "address"
     t.string "telephone_number"
-    t.boolean "is_secede_frag"
+    t.boolean "is_secede_frag", default: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
