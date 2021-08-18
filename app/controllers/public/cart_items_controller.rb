b06721e9358d275ce34f2b9cc85c84ecc73c36e2
hamo-ss <192932aqua@gmail.com>
2021-08-18 04:49:35 +0000

    [Update]cart_items controller

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,2 +1,18 @@
 class Public::CartItemsController < ApplicationController
+  def index
+    @cart_items = CartItem.all
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def destroy_all
+  end
+
 end
