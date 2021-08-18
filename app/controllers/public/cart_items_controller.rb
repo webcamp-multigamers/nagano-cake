9a86363c38ac21dcffe0f324873c590aa45b057e
hamo-ss <192932aqua@gmail.com>
2021-08-18 09:54:02 +0000

    [Update]cart_items controller

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -4,15 +4,39 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
+    @item = Item.find(params[:id])
+    @cart_item = CartItem.new(cart_item_params)
+    @cart_item.customer_id = current_user.id
+    @cart_item.item_id = @item.id
+    unless @cart_item.save
+      render "public/items/show"
+    end
   end
 
   def update
+    cart_item = CartItem.find(params[:item_id])
+    if cart_item.customer_id == current_user.id
+      cart_item.update(cart_item_prams)
+      redirect_to cart_items_path
+    end
   end
 
   def destroy
+    cart_item = CartItem.find(params[:item_id])
+    if cart_item.customer_id == current_user.id
+      cart_item.destroy
+      redirect_to cart_items_path
+    end
   end
 
   def destroy_all
+    current_user.CartItem.destroy_all
+  end
+
+  private
+
+  def cart_item_prams
+    params.require(:cart_item).permit(:quantity)
   end
 
 end
