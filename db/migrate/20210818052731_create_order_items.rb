79c161d904e6aaa4f79b2f3419afe22ec31f73e7
Sanzaka <equation0815@icloud.com>
2021-08-18 05:56:51 +0000

    [add]モデルAddress、OrderItemの追加

---
+++ db/migrate/20210818052731_create_order_items.rb
@@ -0,0 +1,13 @@
+class CreateOrderItems < ActiveRecord::Migration[5.2]
+  def change
+    create_table :order_items do |t|
+      t.integer :item_id, null: false
+      t.integer :order_id, null: false
+      t.integer :order_quantity, null: false
+      t.integer :create_status, default: 0, null: false
+      t.integer :price, null: false
+
+      t.timestamps
+    end
+  end
+end
