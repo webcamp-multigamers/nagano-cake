16e248b8517bfc01fd7d853fe991f42a1a28eb60
yamamoto-hirofumi <sun.lionking.88@gmail.com>
2021-08-20 09:01:57 +0000

    [update]headerルーティングの変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,6 @@ class Item < ApplicationRecord
   attachment :image
   validates  :name, :price, :genre_id, :introduction, presence: true
   validates :is_sales_status, inclusion: {in: [true, false]}
-  validates :quantity, presence: true
 # 税込価格表示のメソッド
    def taxin_price
      (price*1.1).round
