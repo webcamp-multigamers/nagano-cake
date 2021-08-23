1c601dd18ea57f7ec5c18bb936817cd52fcc778b
yamamoto-hirofumi <sun.lionking.88@gmail.com>
2021-08-23 06:09:16 +0000

    変更

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -6,8 +6,26 @@ class Admin::OrdersController < ApplicationController
     @order_items = OrderItem.where(order_id: @order)
   end
   def update
-    @order = Order.find(params[:id])
-    @order_items = @order.order.items
-    @order.update(order_params)
+    binding.pry
+    order = Order.find(params[:id])
+    order_item = OrderItem.find_by(order_id: order.id)
+    order.update(order_params)
+    order_item.update(order_item_params)
+    redirect_to request.referer
+  #   if OrderItem.find_by(order_id: order.id).create_status == "2"
+  #     order.order_status == "制作中"
+  #     order.update
+  # 	end
+  
+  end
+
+
+  private
+  def order_params
+    params.require(:order).permit(:order_status)
+  end
+
+  def order_item_params
+    params.require(:order).permit(:create_status)
   end
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,5 +1,5 @@
 class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :item
-  enum create_status:{ 着手不可: 0, 制作待ち: 1, 製作中: 2, 制作完了: 3 }
+  enum create_status:{ 着手不可: 0, 製作待ち: 1, 製作中: 2, 制作完了: 3 }
 end
