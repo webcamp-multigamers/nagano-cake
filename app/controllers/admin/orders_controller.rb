727ebb85e5f36434f9fcca42f1890454a484b8b7
yamamoto-hirofumi <sun.lionking.88@gmail.com>
2021-08-23 03:02:59 +0000

    変更

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -3,9 +3,11 @@ class Admin::OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
-
+    @order_items = OrderItem.where(order_id: @order)
   end
   def update
     @order = Order.find(params[:id])
+    @order_items = @order.order.items
+    @order.update(order_params)
   end
 end
